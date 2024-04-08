3bc77937c036b0befad810f4d3083ba97e59b270
DARTANN1 <137418497+DARTANN1@users.noreply.github.com>
2024-04-08 21:31:33 +0200

    clone

---
+++ Plugin.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using System.Reflection;
+using Aki.Reflection.Patching;
+using BepInEx;
+using EFT.UI;
+using UnityEngine;
+
+namespace SamSWAT.PS2BIOS
+{
+    [BepInPlugin("com.samswat.ps2bios", "SamSWAT.PS2BIOS", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static AudioClip ButtonOver;
+        internal static AudioClip ButtonClick;
+        
+        private void Awake()
+        {
+            LoadSoundBundle();
+            new Patch().Enable();
+        }
+
+        private void LoadSoundBundle()
+        {
+            var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "/bundles/ps2bios.bundle";
+            var bundle = AssetBundle.LoadFromFile(path);
+            var assets = bundle.LoadAllAssets();
+            ButtonOver = assets[0] as AudioClip;
+            ButtonClick = assets[1] as AudioClip;
+        }
+    }
+    
+    public class Patch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(UISoundsWrapper).GetMethod(nameof(UISoundsWrapper.GetUIClip));
+        }
+
+        [PatchPostfix]
+        private static void PatchPostfix(ref AudioClip __result, EUISoundType soundType)
+        {
+            switch (soundType)
+            {
+                case EUISoundType.ButtonClick:
+                    __result = Plugin.ButtonClick;
+                    break;
+                case EUISoundType.ButtonOver:
+                    __result = Plugin.ButtonOver;
+                    break;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # PS2 BIOS SOUNDS
- replaces button over and click sounds
+replaces button over and click sounds
+
+Reuploaded version of [SamSWAT's Reflex Sights Rework](https://hub.sp-tarkov.com/files/file/1028-ps2-bios-sounds).
+[SamSWAT](https://hub.sp-tarkov.com/user/1652-samswat) - creator of the original mod
+
+## Installation
+After you downloaded .zip archive, unzip it somewhere and drag&drop `SamSWAT.PS2BIOS` folder into the `BepInEx/plugins` folder.
+
+## Disclaimer
+Because SamSWAT's status is M.I.A and orignal downloading link are dead, I wanted to help everyone who's missing this mod.
+When SamSWAT returns, I will stop maintaining this mod and take this page down if requested.
